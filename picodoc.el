4716af64b575f181c633468e40b4fef0eb90499e
tj <tj@data-driven.de>
2013-01-25 16:14:53 +0100

    skeleton for picodoc added.

---
+++ picodoc.el
@@ -0,0 +1,45 @@
+;; * Intro
+
+;; ** Title & Copyright
+
+;; picodoc.el - extract a documentation file from a PicoLisp source file
+;; Version: 0.1
+;; Copyright (c) 2013 Thorsten Jolitz
+;; This file is NOT part of GNU emacs.
+
+;; ** Credits 
+
+;; This library builds on Org-mode, especially Org Babel with its support for
+;; PicoLisp and PlantUML.
+
+
+;; ** Contact:
+
+;; For comments, bug reports, questions, etc, you can contact the author via
+;; email
+
+;; ** License:
+
+;; This work is released under the GPL 2 or (at your option) any later
+;; version.
+
+;; * Requires
+
+
+
+;; * Variable
+;; * Functions
+;; * Outro
+
+(provide 'picodoc)
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; eval: (outline-minor-mode)
+;; eval: (rainbow-mode)
+;; ispell-local-dictionary: "en_US"
+;; outline-regexp: ";; [*]+ "
+;; End:
+
+;;; picodoc.el ends here
