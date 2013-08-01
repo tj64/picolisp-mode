c80a286f4e132b260b0c788ac15b43f2a4a8c292
tj <tj@data-driven.de>
2013-08-01 22:28:45 +0200

    complete restructuring of inferior-picolisp.el, as well as some local formatting changes.

--- inferior-picolisp.el
+++ inferior-picolisp.el
@@ -1,82 +1,35 @@
-;;;;;; inferior-picolisp: Picolisp repl in a buffer.
-;;;;;; Version: 1.2
+;; * inferior-picolisp.el --- picolisp repl in a buffer
+;; ** MetaData
+;;   :PROPERTIES:
+;;   :copyright: Guillermo_R._Palavecino Thorsten_Jolitz
+;;   :copyright-since: 2009
+;;   :version:  1.2
+;;   :licence:  GPL2+
+;;   :licence-url: http://www.gnu.org/licenses/
+;;   :part-of-emacs: no
+;;   :git-repo: https://github.com/tj64/iorg
+;;   :git-clone: git@github.com:tj64/iorg.git
+;;   :authors: Guillermo_R._Palavecino Thorsten_Jolitz
+;;   :contact: <grpala@gmail.com> <tjolitz@gmail.com>
+;;   :inspiration:  cmuscheme.el
+;;   :keywords: emacs picolisp comint repl iorg
+;;   :END:
 
-;;; Copyright (c) 2009, 2012, 2013, Guillermo R. Palavecino, Thorsten Jolitz
+;; ** Commentary
 
-;; This file is NOT part of GNU emacs.
+;; *** Contact
 
-;;;; Credits:
-;; It's and adaptation of GNU emacs' cmuscheme.el
-;;
-;;;; Contact:
-;; For comments, bug reports, questions, etc, you can contact me via IRC
-;; to the user named grpala (or armadillo) on irc.freenode.net in the
-;; #picolisp channel or via email to the author's nickname at gmail.com
-;;
-;;;; License:
-;; This work is released under the GPL 2 or (at your option) any later
-;; version.
+;; For comments, bug reports, questions, etc use the picolisp mailing list,
+;; the #picolisp channel on irc.freenode.net, or the author's emails given
+;; above.
 
+;; * Requires
+ 
 (require 'picolisp)
 (require 'comint)
 
-
-(defgroup picolisp nil
-  "Run an Picolisp process in a buffer."
-  :group 'picolisp )
-
-;;; INFERIOR PICOLISP MODE STUFF
-;;;============================================================================
-
-(defconst inferior-picolisp-version "1.2"
-  "Verion-number of library")
-
-(defcustom inferior-picolisp-mode-hook nil
-  "*Hook for customizing inferior-picolisp mode."
-  :type 'hook
-  :group 'picolisp )
-
-(defvar inferior-picolisp-mode-map
-  (let ((m (make-sparse-keymap)))
-    (define-key m "\M-\C-x" 'picolisp-send-definition) ;gnu convention
-    (define-key m "\C-x\C-e" 'picolisp-send-last-sexp)
-    (define-key m "\C-c\C-l" 'picolisp-load-file)
-    m ) )
-
-(defvar picolisp-local-program-name "./pil +")
-;; (defvar picolisp-process-number 0)
-
-(defvar picolisp-program-name "pil +"
-  "The name of the program used to run Picolisp." )
-
-;; Install the process communication commands in the picolisp-mode keymap.
-(define-key picolisp-mode-map "\M-\C-x" 'picolisp-send-definition);gnu convention
-(define-key picolisp-mode-map "\C-x\C-e" 'picolisp-send-last-sexp);gnu convention
-(define-key picolisp-mode-map "\C-c\C-e" 'picolisp-send-definition)
-(define-key picolisp-mode-map "\C-c\M-e" 'picolisp-send-definition-and-go)
-(define-key picolisp-mode-map "\C-c\C-r" 'picolisp-send-region)
-(define-key picolisp-mode-map "\C-c\M-r" 'picolisp-send-region-and-go)
-(define-key picolisp-mode-map "\C-c\C-x" 'switch-to-picolisp)
-(define-key picolisp-mode-map "\C-c\C-l" 'picolisp-load-file)
-
-(let ((map (lookup-key picolisp-mode-map [menu-bar picolisp])))
-  (define-key map [separator-eval] '("--"))
-  (define-key map [load-file]
-    '("Load Picolisp File" . picolisp-load-file) )
-  (define-key map [switch]
-    '("Switch to Picolisp" . switch-to-picolisp) )
-  (define-key map [send-def-go]
-    '("Evaluate Last Definition & Go" . picolisp-send-definition-and-go) )
-  (define-key map [send-def]
-    '("Evaluate Last Definition" . picolisp-send-definition) )
-  (define-key map [send-region-go]
-    '("Evaluate Region & Go" . picolisp-send-region-and-go) )
-  (define-key map [send-region]
-    '("Evaluate Region" . picolisp-send-region) )
-  (define-key map [send-sexp]
-    '("Evaluate Last S-expression" . picolisp-send-last-sexp) ) )
-
-(defvar picolisp-buffer)
+;; * Mode definitions
+;; ** Inferior Picolisp Mode
 
 (define-derived-mode inferior-picolisp-mode comint-mode "Inferior Picolisp"
   "Major mode for interacting with an inferior Picolisp process.
@@ -84,31 +37,46 @@
 The following commands are available:
 \\{inferior-picolisp-mode-map}
 
-An Picolisp process can be fired up with M-x run-picolisp.
+An Picolisp process can be fired up with 'M-x run-picolisp'.
 
-Customization: Entry to this mode runs the hooks on comint-mode-hook and
-inferior-picolisp-mode-hook (in that order).
+Customization: Entry to this mode runs the hooks on `comint-mode-hook' and
+`inferior-picolisp-mode-hook' (in that order).
 
-You can send text to the inferior Picolisp process from other buffers containing
-Picolisp source.
-    switch-to-picolisp switches the current buffer to the Picolisp process buffer.
-    picolisp-send-definition sends the current definition to the Picolisp process.
-    picolisp-send-region sends the current region to the Picolisp process.
+You can send text to the inferior Picolisp process from other
+buffers containing Picolisp source.
 
-    picolisp-send-definition-and-go and picolisp-send-region-and-go
-        switch to the Picolisp process buffer after sending their text.
+ - `switch-to-picolisp' switches the current buffer to the
+   Picolisp process buffer.
+
+ - `picolisp-send-definition' sends the current definition to the
+   Picolisp process.
+
+ - `picolisp-send-region' sends the current region to the
+   Picolisp process.
+
+ - `picolisp-send-definition-and-go' and
+   `picolisp-send-region-and-go' switch to the Picolisp process
+   buffer after sending their text.
+
 For information on running multiple processes in multiple buffers, see
-documentation for variable picolisp-buffer.
+documentation for variable `picolisp-buffer'.
 
 Commands:
-Return after the end of the process' output sends the text from the
-    end of process to point.
-Return before the end of the process' output copies the sexp ending at point
-    to the end of the process' output, and sends it.
-Delete converts tabs to spaces as it moves back.
-Tab indents for Picolisp; with argument, shifts rest
-    of expression rigidly with the current line.
-C-M-q does Tab on each line starting within following expression.
+
+'Return' after the end of the process' output sends the text from
+the end of process to point.
+
+'Return' before the end of the process' output copies the sexp
+ending at point to the end of the process' output, and sends it.
+
+'Delete' converts tabs to spaces as it moves back.
+
+'Tab' indents for Picolisp; with argument, shifts rest of
+expression rigidly with the current line.
+
+'C-M-q' does Tab on each line starting within following
+expression.
+
 Paragraphs are separated only by blank lines.  Semicolons start comments.
 If you accidentally suspend your process, use \\[comint-continue-subjob]
 to continue it."
@@ -121,11 +89,113 @@ to continue it."
   (setq mode-line-process '(":%s"))
   (setq comint-input-ring-file-name "~/.pil_history") )
 
+;; * Hooks
+;; * Variables
+;; ** Vars
+
+(defvar picolisp-local-program-name "./pil +")
+;; (defvar picolisp-process-number 0)
+
+(defvar picolisp-program-name "pil +"
+  "The name of the program used to run Picolisp." )
+
+(defvar picolisp-buffer)
+
+(defvar picolisp-prev-load-dir/file nil
+  "Caches the last (directory . file) pair.
+Caches the last pair used in the last `picolisp-load-file' command.
+Used for determining the default in the next one." )
+
+
+(defvar picolisp-buffer nil "*The current picolisp process buffer.
+
+MULTIPLE PROCESS SUPPORT
+==================================================================
+
+inferior-picolisp.el supports, in a fairly simple fashion,
+running multiple Picolisp processes. To run multiple Picolisp
+processes, you start the first up with \\[run-picolisp]. It will
+be in a buffer named *picolisp*. Rename this buffer with
+\\[rename-buffer]. You may now start up a new process with
+another \\[run-picolisp]. It will be in a new buffer, named
+*picolisp*. You can switch between the different process buffers
+with \\[switch-to-buffer].
+
+Commands that send text from source buffers to Picolisp processes
+-- like `picolisp-send-definition' -- have to choose a process to
+send to, when you have more than one Picolisp process around.
+This is determined by the global variable `picolisp-buffer'.
+Suppose you have three inferior Picolisps running:
+
+    Buffer      Process
+    foo         picolisp
+    bar         picolisp<2>
+    *picolisp*  picolisp<3>
+
+If you do a \\[picolisp-send-definition-and-go] command on some
+Picolisp source code, what process do you send it to?
+
+- If you're in a process buffer (foo, bar, or *picolisp*),
+  you send it to that process.
+- If you're in some other buffer (e.g., a source file), you
+  send it to the process attached to buffer `picolisp-buffer'.
+
+This process selection is performed by function `picolisp-proc'.
+
+Whenever \\[run-picolisp] fires up a new process, it resets
+`picolisp-buffer' to be the new process's buffer. If you only run
+one process, this will do the right thing. If you run multiple
+processes, you can change `picolisp-buffer' to another process
+buffer with \\[set-variable].
+
+More sophisticated approaches are, of course, possible. If you
+find yourself needing to switch back and forth between multiple
+processes frequently, you may wish to consider ilisp.el, a
+larger, more sophisticated package for running inferior Lisp and
+Picolisp processes. The approach taken here is for a minimal,
+simple implementation. Feel free to extend it." )
+
+;; ** Consts
+
+(defconst inferior-picolisp-version "1.2"
+  "Verion-number of library")
+
+;; ** Customs
+;; *** Custom Groups
+
+(defgroup picolisp nil
+  "Run an Picolisp process in a buffer."
+  :group 'picolisp )
+
+;; *** Custom Vars
+
+(defcustom inferior-picolisp-mode-hook nil
+  "*Hook for customizing inferior-picolisp mode."
+  :type 'hook
+  :group 'picolisp )
+
+(defcustom inferior-picolisp-load-hook nil
+  "This hook is run when inferior-picolisp is loaded in.
+This is a good place to put keybindings."
+  :type 'hook
+  :group 'picolisp )
+
 (defcustom inferior-picolisp-filter-regexp "\\`\\s *\\S ?\\S ?\\s *\\'"
   "*Input matching this regexp are not saved on the history list.
 Defaults to a regexp ignoring all inputs of 0, 1, or 2 letters."
   :type 'regexp
   :group 'picolisp )
+
+(defcustom picolisp-source-modes '(picolisp-mode)
+  "*Used to determine if a buffer contains Picolisp source code.
+If it's loaded into a buffer that is in one of these major modes,
+it's considered a picolisp source file by `picolisp-load-file'.  Used by
+these commands to determine defaults."
+  :type '(repeat function)
+  :group 'picolisp )
+
+;; * Functions
+;; ** Non-interactive Functions
 
 (defun picolisp-input-filter (str)
   "Don't save anything matching `inferior-picolisp-filter-regexp'."
@@ -190,6 +260,7 @@ The line-editor is not needed when PicoLisp is run as Emacs subprocess."
       (delete-file
        (expand-file-name "editor" pil-tmp-dir)))))
 
+;; ** Commands
 
 ;;;###autoload
 (defun run-picolisp-new (cmd &optional iorg-scrape-mode-p)
@@ -224,7 +295,6 @@ Input and output via buffer `*picolisp*<N>' or
         (iorg-scrape-mode)
       (inferior-picolisp-mode)))
     (pop-to-buffer (current-buffer)) )
-
 
 ;;;###autoload
 (defun run-picolisp (cmd)
@@ -314,71 +384,20 @@ Then switch to the process buffer."
   (picolisp-send-definition)
   (switch-to-picolisp t) )
 
-(defcustom picolisp-source-modes '(picolisp-mode)
-  "*Used to determine if a buffer contains Picolisp source code.
-If it's loaded into a buffer that is in one of these major modes,
-it's considered a picolisp source file by `picolisp-load-file'.  Used by
-these commands to determine defaults."
-  :type '(repeat function)
-  :group 'picolisp )
-
-(defvar picolisp-prev-load-dir/file nil
-  "Caches the last (directory . file) pair.
-Caches the last pair used in the last `picolisp-load-file' command.
-Used for determining the default in the next one." )
-
 (defun picolisp-load-file (file-name)
   "Load a Picolisp file FILE-NAME into the inferior Picolisp process."
-  (interactive (comint-get-source "Load Picolisp file: " picolisp-prev-load-dir/file
-                                  picolisp-source-modes t ) ) ; t because `load'
-                                                       ; needs an exact name
-  (comint-check-source file-name) ; Check to see if buffer needs saved.
-  (setq picolisp-prev-l/c-dir/file (cons (file-name-directory    file-name)
-                                       (file-name-nondirectory file-name) ) )
-  (comint-send-string (picolisp-proc) (concat "(load \""
-                                            file-name
-                                            "\"\)\n" ) ) )
-
-
-(defvar picolisp-buffer nil "*The current picolisp process buffer.
-
-MULTIPLE PROCESS SUPPORT
-===========================================================================
-inferior-picolisp.el supports, in a fairly simple fashion, running multiple Picolisp
-processes.  To run multiple Picolisp processes, you start the first up with
-\\[run-picolisp].  It will be in a buffer named *picolisp*.  Rename this buffer
-with \\[rename-buffer].  You may now start up a new process with another
-\\[run-picolisp].  It will be in a new buffer, named *picolisp*.  You can
-switch between the different process buffers with \\[switch-to-buffer].
-
-Commands that send text from source buffers to Picolisp processes -- like
-`picolisp-send-definition' -- have to choose a process to send to, when you
-have more than one Picolisp process around.  This is determined by the
-global variable `picolisp-buffer'.  Suppose you have three inferior Picolisps
-running:
-    Buffer      Process
-    foo         picolisp
-    bar         picolisp<2>
-    *picolisp*  picolisp<3>
-If you do a \\[picolisp-send-definition-and-go] command on some Picolisp source
-code, what process do you send it to?
-
-- If you're in a process buffer (foo, bar, or *picolisp*),
-  you send it to that process.
-- If you're in some other buffer (e.g., a source file), you
-  send it to the process attached to buffer `picolisp-buffer'.
-This process selection is performed by function `picolisp-proc'.
-
-Whenever \\[run-picolisp] fires up a new process, it resets `picolisp-buffer'
-to be the new process's buffer.  If you only run one process, this will
-do the right thing.  If you run multiple processes, you can change
-`picolisp-buffer' to another process buffer with \\[set-variable].
-
-More sophisticated approaches are, of course, possible.  If you find yourself
-needing to switch back and forth between multiple processes frequently,
-you may wish to consider ilisp.el, a larger, more sophisticated package
-for running inferior Lisp and Picolisp processes.  The approach taken here is
-for a minimal, simple implementation.  Feel free to extend it." )
+  (interactive
+   (comint-get-source "Load Picolisp file: "
+                      picolisp-prev-load-dir/file
+                      ;; t because `load' needs an exact name
+                      picolisp-source-modes t ) )
+  ;; Check to see if buffer needs saved.
+  (comint-check-source file-name) 
+  (setq picolisp-prev-l/c-dir/file
+        (cons (file-name-directory file-name)
+              (file-name-nondirectory file-name) ) )
+  (comint-send-string
+   (picolisp-proc) (concat "(load \"" file-name "\"\)\n" ) ) )
 
 (defun picolisp-proc ()
   "Return the current Picolisp process, starting one if necessary.
@@ -403,14 +422,51 @@ command to run."
   (save-window-excursion
     (run-picolisp (read-string "Run Picolisp: " picolisp-program-name)) ) )
 
-;;; Do the user's customization...
+;; * Menus and Keys
+;; ** Mode Map
 
-(defcustom inferior-picolisp-load-hook nil
-  "This hook is run when inferior-picolisp is loaded in.
-This is a good place to put keybindings."
-  :type 'hook
-  :group 'picolisp )
+(defvar inferior-picolisp-mode-map
+  (let ((m (make-sparse-keymap)))
+    (define-key m "\M-\C-x" 'picolisp-send-definition) ;gnu convention
+    (define-key m "\C-x\C-e" 'picolisp-send-last-sexp)
+    (define-key m "\C-c\C-l" 'picolisp-load-file)
+    m ) )
 
+;; ** Menus
+
+(let ((map (lookup-key picolisp-mode-map [menu-bar picolisp])))
+  (define-key map [separator-eval] '("--"))
+  (define-key map [load-file]
+    '("Load Picolisp File" . picolisp-load-file) )
+  (define-key map [switch]
+    '("Switch to Picolisp" . switch-to-picolisp) )
+  (define-key map [send-def-go]
+    '("Evaluate Last Definition & Go" . picolisp-send-definition-and-go) )
+  (define-key map [send-def]
+    '("Evaluate Last Definition" . picolisp-send-definition) )
+  (define-key map [send-region-go]
+    '("Evaluate Region & Go" . picolisp-send-region-and-go) )
+  (define-key map [send-region]
+    '("Evaluate Region" . picolisp-send-region) )
+  (define-key map [send-sexp]
+    '("Evaluate Last S-expression" . picolisp-send-last-sexp) ) )
+
+;; ** Keys
+
+;; Install the process communication commands in the picolisp-mode keymap.
+;; gnu convention
+(define-key picolisp-mode-map "\M-\C-x" 'picolisp-send-definition)
+;; gnu convention
+(define-key picolisp-mode-map "\C-x\C-e" 'picolisp-send-last-sexp)
+(define-key picolisp-mode-map "\C-c\C-e" 'picolisp-send-definition)
+(define-key picolisp-mode-map "\C-c\M-e" 'picolisp-send-definition-and-go)
+(define-key picolisp-mode-map "\C-c\C-r" 'picolisp-send-region)
+(define-key picolisp-mode-map "\C-c\M-r" 'picolisp-send-region-and-go)
+(define-key picolisp-mode-map "\C-c\C-x" 'switch-to-picolisp)
+(define-key picolisp-mode-map "\C-c\C-l" 'picolisp-load-file)
+
+
+;; * Run hooks and provide
 (run-hooks 'inferior-picolisp-load-hook)
 
 (provide 'inferior-picolisp)
