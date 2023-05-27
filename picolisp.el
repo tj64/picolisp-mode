52377f48408d06f204b730a9f016eb43d38e7ce8
cryptorick <cryptorick@users.noreply.github.com>
2023-05-27 15:20:32 -0400

    make package.el's package-buffer-info happy

--- picolisp.el
+++ picolisp.el
@@ -1,4 +1,4 @@
-;;; picolisp-mode --- Major mode to edit picoLisp.
+;;; picolisp.el --- Major mode to edit picoLisp.
 ;; Version: 1.3
 
 ;; Copyright (c) 2009, Guillermo R. Palavecino
@@ -761,3 +761,5 @@ The main differences are:
    (font-lock-add-keywords 'inferior-picolisp-mode tsm-lock) ) ) 
 
 (provide 'picolisp)
+
+;;; picolisp.el ends here
